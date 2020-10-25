3a38a60b74392e50cd4c2a2704cf53ee88625c18
Shashank <srirangamshashank@gmail.com>
2020-10-25 12:58:43 +0530

    pre_processing done till generating the sparse matrix

--- src/pre_processing.py
+++ src/pre_processing.py
@@ -7,15 +7,28 @@ def map_shingles(fname,n_gram):
     documents=data['sequence']
     shingle_codes={}
     all_shingles=set()
+    shingle_set=[set() for _ in range(0,len(documents))]
+    doc_count=0
     for doc in documents:
         shingles=shingling(doc,n_gram)
+        shingle_set[doc_count].update(shingles)
         all_shingles.update(shingles)
+        doc_count=doc_count+1
     count=0
     for shingle in all_shingles:
         shingle_codes[shingle]=count
         count=count+1
-    return (shingle_codes,count)
+    return (shingle_codes,count,shingle_set)
 
+def gen_matrix(shingle_codes,count,shingle_set):
+    sparse_matrix=np.zeros((count+1,len(shingle_set)))
+    for i in range(0,len(shingle_set)):
+        for shingles in shingle_set[i]:
+            sparse_matrix[shingle_codes[shingles]][i]=1
+    return sparse_matrix
+
+   
+    
             
         
         
